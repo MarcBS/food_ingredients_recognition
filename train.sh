f7e1f06d202eecc44e58de2279a7092f9ed4fe34
Marc Bolaños Solà <marc.bolanos@ub.edu>
2017-03-01 09:01:22 +0100

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-PYTHONPATH=$PYTHONPATH:/media/HDD_2TB/marc/multimodal_keras_wrapper python -u main.py
+PYTHONPATH=$PYTHONPATH:/media/HDD_2TB/marc/multimodal_keras_wrapper:~/code/keras python -u main.py
